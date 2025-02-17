25526ab0e956cf3b77cda68786e79be44c46dca8
Leandro Santos <ldss.leandro0212@gmail.com>
2025-02-17 19:01:42 -0300

    test: :test_tube: Ajuste no arquivo de teste

--- math_test.go
+++ math_test.go
@@ -2,11 +2,11 @@ package main
 
 import "testing"
 
-func Testsoma(t *testing.T) {
+func TestSoma(t *testing.T) {
 
 	total := soma(15, 15)
 
 	if total != 30 {
-		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d', total, 30")
+		t.Errorf("Resultado da some é inválido: Resultado %d. Esperado: %d", total, 30)
 	}
 }
